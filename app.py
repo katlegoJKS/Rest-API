64321900765f1f08980bbc19abd30d44b68c08ec
katlegoJKS <katlego.malatjjie00@gmail.com>
2020-04-03 11:11:22 +0200

    404- Not found

--- app.py
+++ app.py
@@ -35,15 +35,72 @@ class Computer(db.Model):
         self.hard_drive_space = hard_drive_space
         self.form_factor = form_factor
 
-# ComputerTable or ComputerSchema
-class ComputerTable(ma.Schema):
+# PostSchema or ComputerSchema
+class PostSchema(ma.Schema):
     class Meta:
-        fields = ('ComputerID', 'hard_drive_type', 'processor', 'amount_of_ram', 'maximum_ram', 'hard_drive_space', 'form_factor')
+        fields = ('hard_drive_type', 'processor', 'amount_of_ram', 'maximum_ram', 'hard_drive_space', 'form_factor')
+
 
 
 #Initialise Table/Schema
-computer_schema = ComputerTable(strict=True)
-computers_schema = ComputerTable(many=True, strict=True)
+post_schema = PostSchema(strict=True)
+posts_schema = PostSchema(many=True, strict=True)
+
+@app.route('/post', methods= ['POST'])
+def add_post():
+    hard_drive_type = request.json['hard_drive_type']
+    processor = request.json['processor']
+    amount_of_ram = request.json['amount_of_ram']
+    maximum_ram = request.json['maximum_ram']
+    hard_drive_space = request.json['hard_drive_space']
+    form_factor = request.json['form_factor']
+
+    my_posts = Computer(hard_drive_type, processor, amount_of_ram, maximum_ram, hard_drive_space,form_factor)
+    db.session.add(my_posts)
+    db.session.commit()
+
+    return post_schema.jsonify(my_posts)
+
+@app.route('/get', methods = ['GET'])
+def get_post():
+    all_posts = Computer.query.all()
+    result = posts_schema.dump(all_posts)
+
+    return jsonify(result)
+
+@app.route('/post_deails/<ComputerID>', methods = ['GET'])
+def post_details(ComputerID):
+    post = Computer.query.get(CustomerID)
+    return Post_schema.jsonify(post)
+
+@app.route('/post_update/<ComputerID>/', methods= ['PUT'])
+def post_update(ComputerID):
+    post = Computer.query.get(CustomerID)
+
+    hard_drive_type = request.json['hard_drive_type']
+    processor = request.json['processor']
+    amount_of_ram = request.json['amount_of_ram']
+    maximum_ram = request.json['maximum_ram']
+    hard_drive_space = request.json['hard_drive_space']
+    form_factor = request.json['form_factor']
+
+    post.hard_drive_type = hard_drive_type
+    post.processor = processor
+    post.amount_of_ram = amount_of_ram
+    post.maximum_ram = maximum_ram
+    post.hard_drive_space = hard_drive_space
+    post.form_factor = form_factor
+
+    db.session.commit()
+    return post_schema.jsonify(post)
+
+@app.route('/post_delete/<ComputerID>', methods = ['DELETE'])
+def post_delete(ComputerID):
+    post = Computer.query.get(CustomerID)
+    db.session.delete(post)
+    db.session.commit()
+
+    return Post_schema.jsonify(post)
 
 if __name__ == '__main__':
     app.run(debug=True)
